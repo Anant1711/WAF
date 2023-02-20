c74fa7c87ab57dca57a07707faeafacf0e4291d2
Anant chauhan <63709820+Anant1711@users.noreply.github.com>
2023-02-20 16:21:53 +0530

    app.js File

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var path=require("path");   
+var routes=require("./route");
+var app = express();
+const port = process.env.PORT || 3002;
+app.set('views', path.join(__dirname,"views"));
+app.set('view engine','ejs');
+
+app.use(routes);
+
+app.listen(port, () => console.log(`Your WAF app listening on port ${port}!`));
