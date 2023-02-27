7fef9020f241a1ba5067fd5728370aa3d9e36a70
KikixJ7 <94973365+KikixJ7@users.noreply.github.com>
2023-02-27 20:26:58 +0400

    Update app.js

--- WAFTest/app.js
+++ WAFTest/app.js
@@ -1,11 +1,30 @@
-var express = require("express");
-var path=require("path");   
-var routes=require("./route");
-var app = express();
+const request = require('request');
+const express = require("express");
+const path=require("path");   
+const indexRoutes =require("./route.js");
+const app = express();
 const port = process.env.PORT || 3002;
+const { createProxyMiddleware } = require('http-proxy-middleware');
 app.set('views', path.join(__dirname,"views"));
 app.set('view engine','ejs');
-
-app.use(routes);
-
-app.listen(port, () => console.log(`Your WAF app listening on port ${port}!`));
+//console.log(indexRoutes)
+// Forward all requests to the WAF on port 3001
+app.use((req,res,next)=>{
+  const appReq = request({
+    uri: 'http://localhost:3001' + req.url,
+    headers: req.headers
+  });
+  req.pipe(appReq);
+  appReq.on('response', (appRes) => {
+    //console.log(appRes)
+    if(appRes.statusCode === 200 || appRes.statusCode === 304){
+      next();
+    }else{
+      res.status(appRes.statusCode).sendStatus(appRes.statusCode);
+      //router.get("*", (req, res) => {
+       // res.render('404page');
+     // })
+    }    
+    
+  });
+});
