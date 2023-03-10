f9660b322c8cd0b890459e7d60951ac3391d046d
Arpan Thomas <66914302+Arpanthomas1121@users.noreply.github.com>
2023-03-10 15:23:52 +0530

    Update app.js
    code for easy scaling the app

--- WAFTest/app.js
+++ WAFTest/app.js
@@ -7,24 +7,14 @@ const port = process.env.PORT || 3002;
 const { createProxyMiddleware } = require('http-proxy-middleware');
 app.set('views', path.join(__dirname,"views"));
 app.set('view engine','ejs');
-//console.log(indexRoutes)
-// Forward all requests to the WAF on port 3001
-app.use((req,res,next)=>{
-  const appReq = request({
-    uri: 'http://localhost:3001' + req.url,
-    headers: req.headers
-  });
-  req.pipe(appReq);
-  appReq.on('response', (appRes) => {
-    //console.log(appRes)
-    if(appRes.statusCode === 200 || appRes.statusCode === 304){
-      next();
-    }else{
-      res.status(appRes.statusCode).sendStatus(appRes.statusCode);
-      //router.get("*", (req, res) => {
-       // res.render('404page');
-     // })
-    }    
-    
-  });
-});
+app.use(indexRoutes)
+
+// toggle below code based on your deployed environment
+//code for cloud web server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// code for local
+
+// app.listen(3002, 'localhost', () => {
+//   console.log('Backend server listening on port 3002');
+// });
